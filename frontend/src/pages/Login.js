add05de10ec88eda374890b7142558ddbe1d1b76
Bhuvana123398 <bhke22csds@cmrit.ac.in>
2026-08-20 16:52:55 +0530

    Removed localhost and switched to production API URL

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -9,7 +9,7 @@ const Login = ({ setUser }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', formData);
+      const res = await axios.post(`${API_URL}/auth/login`, formData);
       localStorage.setItem('token', res.data.token);
       localStorage.setItem('user', JSON.stringify(res.data.user));
       setUser(res.data.user);
