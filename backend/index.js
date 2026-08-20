bb620b49d6d9e3cb51f07204279be05fcbc45659
Bhuvana123398 <bhke22csds@cmrit.ac.in>
2026-08-20 21:35:48 +0530

    Change mobile req

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const app = express();
 app.use(cors({
-    origin: true,
+    origin: '*',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
